a1b30d510d7eeb4ae773e061375ad359a3876587
seno-ark <amin.kamsena@gmail.com>
2024-02-29 10:41:26 +0700

    add handler validator

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,15 +4,18 @@ import (
 	"notes-api/internal"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
 )
 
 type handler struct {
-	usecase internal.NoteUsecase
+	usecase  internal.NoteUsecase
+	validate *validator.Validate
 }
 
 func NewHandler(usecase internal.NoteUsecase) *handler {
 	return &handler{
-		usecase: usecase,
+		usecase:  usecase,
+		validate: validator.New(),
 	}
 }
 
